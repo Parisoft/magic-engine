0929d107877cba0cad71ea2891c553f38ec7e33f
andre-paris <andre.paris@gmail.com>
2016-01-19 19:44:54 -0200

    shortcuts

--- src/main/java/com/github/parisoft/magic/engine/game/Games.java
+++ src/main/java/com/github/parisoft/magic/engine/game/Games.java
@@ -1,11 +1,25 @@
 package com.github.parisoft.magic.engine.game;
 
 import com.github.parisoft.magic.engine.entity.Player;
+import com.github.parisoft.magic.engine.game.phase.Phase;
+import com.github.parisoft.magic.engine.game.step.Step;
 
 public class Games {
 
     public static Game currentGame() {
         return Match.getInstance().getCurrentGame();
+    }
+    
+    public static Turn currentTurn() {
+        return currentGame().getCurrentTurn();
+    }
+    
+    public static Phase currentPhase() {
+        return currentTurn().getCurrentPhase();
+    }
+    
+    public static Step currentStep() {
+        return currentPhase().getCurrentStep();
     }
     
     public static Player activePlayer() {
